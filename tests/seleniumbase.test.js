9da9f8b6099ea4e5663e16e45d8a3250c1221839
leandjb <leandjb@gmail.com>
2025-01-28 20:33:43 -0500

    tag test fixed

--- tests/seleniumbase.test.js
+++ tests/seleniumbase.test.js
@@ -1,16 +1,18 @@
 const { test, expect} = require('@playwright/test')
 
+
 test.describe('SeleniumBase Page - Test Suite', ()=> {
 
+
   test.beforeEach(async ({ page }) => {
-    
     await page.goto('https://seleniumbase.io/demo_page/')
   })
 
-  test.only('input text field test', async ({ page })=> {
-    
-    await page.locator('#myTextInput').fill('wrote line')
 
+  test('input text field test', async ({ page })=> {    
+    await page.locator('#myTextInput').fill('wrote line')
     expect(page.locator('#myTextInput')).toHaveValue('wrote line')
   })
+
+
 })
